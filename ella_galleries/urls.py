60db3d671abc18cf5126c66a6e61bd3c65d50ac7
michal dub <michal.dub@sanomamedia.cz>
2015-02-05 11:46:09 +0100

    revert lazy_regex in urls

--- ella_galleries/urls.py
+++ ella_galleries/urls.py
@@ -1,7 +1,5 @@
 from django.template.defaultfilters import slugify
-from django.utils.translation import ugettext_lazy as _
-from django.utils.functional import lazy
-
+from django.utils.translation import ugettext
 try:
     from django.conf.urls import patterns, url
 except:
@@ -12,21 +10,10 @@ from ella.core.custom_urls import resolver
 from ella_galleries.views import gallery_item_detail
 from ella_galleries.models import Gallery
 
-lazy_slugify = lazy(slugify, str)
-lazy_regex = lazy(lambda regex, res_dict: regex % res_dict, str)
-
-res = {
-    'item_slug': r'(?P<item_slug>[\w-]+)',
-    'url_remainder': r'(?P<url_remainder>.+/)',
-    'item_prefix': lazy_slugify(_('Item')),
-}
-
-
-urlpatterns = patterns(
-    '',
-    url(lazy_regex(r'^%(item_prefix)s/%(item_slug)s/%(url_remainder)s$', res), gallery_item_detail, name='gallery-item-detail-custom'),
-    url(lazy_regex(r'^%(item_prefix)s/%(item_slug)s/$', res), gallery_item_detail, name='gallery-item-detail'),
+urlpatterns = patterns('',
+    url(r'^(?P<item_slug>[\w-]+)/(?P<url_remainder>.+/)$', gallery_item_detail, name='gallery-item-detail-custom'),
+    url(r'^(?P<item_slug>[\w-]+)/$', gallery_item_detail, name='gallery-item-detail'),
 )
 
-resolver.register(urlpatterns, model=Gallery)
+resolver.register(urlpatterns, prefix=slugify(ugettext('Item')), model=Gallery)
 resolver.register_custom_detail(Gallery, gallery_item_detail)
